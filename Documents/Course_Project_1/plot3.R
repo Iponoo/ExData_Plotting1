34d3fcce60c677357ce9079ce09980d24741bc16
Iponoo <fico@inbox.com>
2015-05-08 18:18:01 +0200

    Update plot3.R

--- Documents/Course_Project_1/plot3.R
+++ Documents/Course_Project_1/plot3.R
@@ -23,5 +23,5 @@ legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2,
        legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 
 ## Saving to file
-##dev.copy(png, file="plot3.png", height=480, width=480) I used png(), no problems with resize.
+##dev.copy(png, file="plot3.png", height=480, width=480) -> I used png(), no problems with resize.
 dev.off()
